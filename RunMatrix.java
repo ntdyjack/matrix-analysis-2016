d4910f3597e9309d5d00c69698c50080e29ba443
ntdyjack <nathan.dyjack12@ncf.edu>
2016-02-04 20:44:33 -0500

    fuck nik

---
+++ RunMatrix.java
@@ -0,0 +1,13 @@
+package matrixanalysis;
+
+public class RunMatrix {
+
+	public static void main(String[] args) {
+		double[][] d = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1} };
+        Matrix D = new Matrix(d);
+        D.show();        
+        //System.out.println();
+
+	}
+
+}
---
+++ Vector.java
@@ -0,0 +1,10 @@
+package matrixanalysis;
+
+final public class Vector {
+
+    private final int M;             // number of rows
+    private final int N;             // number of columns
+    private final double[][] data;   // M-by-N array
+	
+	
+}
