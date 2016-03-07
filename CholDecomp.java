41a331a66d8626b79a7b4a92fd7612ee915d9e33
nikwoj <nikwoj@gmail.com>
2016-03-07 04:02:53 -0500

    Started and finished Cholesky Decomposition. Also added det method to LU to compute determinant.

---
+++ CholDecomp.java
@@ -0,0 +1,51 @@
+package matrixanalysis;
+
+public class CholDecomp {
+	
+	private final Matrix L;
+	private final Matrix V;
+	
+	public CholDecomp(Matrix V) {
+		this.V = V;
+		int N = V.N();
+		this.L = new Matrix(N,N);
+	}
+	
+	public Matrix L() { return this.L; }
+	
+	public void factor() {
+		double sum = 0;
+		int i, j, k;
+		int N = this.V.N();
+		
+		for (i=0; i < N; ++i) { // What row are we looking at
+			for (j=i; j < N; ++j) { // What column are we looking at
+				sum = 0;
+				if (i==j) {	
+					for (k=0; k<i; ++k )  // Sum up previous entries
+						sum += L.data[i][k] * L.data[i][k];
+					L.data[i][i] = Math.sqrt(this.V.data[i][i] - sum);
+				}
+				else {
+					for (k=0; k<i; ++k)
+						sum +=  L.data[i][k] * L.data[j][k];
+					L.data[j][i] = (this.V.data[i][j] - sum) / L.data[i][i];
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Matrix A = Matrix.random(5,5);
+		A = A.times(A.T());
+		A.show();
+		System.out.println();
+		
+		CholDecomp CD = new CholDecomp(A);
+		CD.factor();
+		Matrix L = CD.L();
+		L.show();
+		System.out.println();
+		L.times(L.T()).show();
+	}
+}
